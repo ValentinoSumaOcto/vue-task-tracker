bbe4befeb62b523a5991637f5d7ed44681f6fa39
“valentino <valentinosuma@gmail.com>
2021-05-28 12:02:04 +0200

    validation form

--- vue-crash-2021/src/App.vue
+++ vue-crash-2021/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
+    <AddTask @add-task="addTask" />
     <Tasks
       @toggle-reminder="toggleReminder"
       @delete-task="deleteTask"
@@ -12,19 +13,26 @@
 <script>
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 export default {
   name: "App",
   components: {
     Header,
     Tasks,
+    AddTask,
   },
   data() {
     return {
       tasks: [],
+      showAddTask: false
     };
   },
   methods: {
+    addTask(newTask) {
+      this.tasks = [...this.tasks, newTask];
+    },
+
     deleteTask(id) {
       // console.log('task', id)
       if (confirm("Are you sure?")) {
